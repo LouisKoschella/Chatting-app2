b09385803973ad82ff259b528289f7d5cc496c01
Miroslava Buchtíková <72465150+MiroslavaBuchtikova@users.noreply.github.com>
2023-06-08 12:10:04 +0200

    Update WhenPostMessage.cs

--- Chatting-app2/WhenPostMessage.cs
+++ Chatting-app2/WhenPostMessage.cs
@@ -1,36 +1,45 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Graph.Models;
-using Chatting_app2.Test;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Graph.Models.Security;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.TestHost;
 using Xunit;
-
 
 namespace Chatting_app2
 {
-
     public class WhenPostMessage
     {
         [Fact]
         public async Task ShouldReturnCorrectResponse()
         {
-            
             // Arrange
-            await using var context = new MockedDb().CreateDbContext();
+
 
             // Act
-            await using var application = new WebApplicationFactory<Program>();
+            await using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton(_ =>
+                    {
+                        var dbContext = new MockedDb().CreateDbContext();
+                        return dbContext;
+                    });
+
+                    services.BuildServiceProvider();
+                });
+            });
+            ;
 
             using var client = application.CreateClient();
-            var result = await client.PostAsJsonAsync("/Message", new MessageDTO { MessageText = "test"});
-            var content = await result.Content.ReadFromJsonAsync<Message>();
+            var result = await client.PostAsJsonAsync("/Message", new MessageDTO
+            {
+                MessageText = "test",
+                Username = "testusernmae",
+                MessageTime = DateTime.Now
+            });
+            var content = await result.Content.ReadFromJsonAsync<MessageDTO>();
+
             //Assert
-
-            //Assert.AreEqual("test", content?. );
-
-            Xunit.Assert.NotNull(content);
+            Assert.NotNull(content);
+            Assert.Equal("test", content?.MessageText);
         }
     }
 }
